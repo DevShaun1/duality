19f62d094fbe72ace23728d530420c86508c1f70
Shaun Ziman <devshaun1@gmail.com>
2026-06-15 18:54:25 +0200

    Refactor routing structure: Implement PublicLayout and ProtectedRoute components to manage authentication-based access to routes. Update AppRoutes to conditionally render components based on user login state, enhancing navigation and user experience.

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,4 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "@/pages/Home";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "@/pages/Dashboard";
 import Signup from "@/pages/Signup";
 import Login from "@/pages/Login";
@@ -7,17 +6,34 @@ import Reflection from "@/pages/Reflection";
 import WeeklyReview from "@/pages/WeeklyReview";
 import NotFound from "@/pages/NotFound";
 import History from "@/pages/History";
+import PublicLayout from "@/components/layout/PublicLayout";
+import AppLayout from "@/components/layout/AppLayout";
+import ProtectedRoute from "./ProtectedRoute";
+import { useAuth } from "@/features/auth/AuthProvider";
 
 export function AppRoutes() {
+  const { isLoggedIn } = useAuth();
+
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/reflection/new" element={<Reflection />} />
-      <Route path="/weekly-review" element={<WeeklyReview />} />
-      <Route path="/history" element={<History />} />
+       <Route element={<PublicLayout isLoggedIn={isLoggedIn} />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+       </Route>
+
+       <Route element={<ProtectedRoute />}>
+        <Route element={<AppLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/reflection" element={<Reflection />} />
+          <Route path="/weekly-review" element={<WeeklyReview />} />
+          <Route path="/history" element={<History />} />
+        </Route>
+      </Route>
+
+      <Route path="/" element={
+        <Navigate to={isLoggedIn ? '/reflection' : '/login'} replace />
+      }/>
+
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
