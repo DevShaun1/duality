72c7ca4bd61cf3d2f5a5056fa65be13c7e1bd8a4
Shaun Ziman <devshaun1@gmail.com>
2026-06-24 21:43:32 +0200

    Refactor DevtoolProvider: Simplify return statement for improved readability.

--- src/providers/DevtoolProvider.tsx
+++ src/providers/DevtoolProvider.tsx
@@ -2,14 +2,5 @@ import { TanStackDevtools } from '@tanstack/react-devtools';
 import { ReactQueryDevtoolsPanel } from '@tanstack/react-query-devtools';
 
 export default function DevtoolProvider() {
-  return (
-    <TanStackDevtools
-      plugins={[
-        {
-          name: 'TanStack Query',
-          render: <ReactQueryDevtoolsPanel />,
-        },
-      ]}
-    />
-  );
+  return <TanStackDevtools plugins={[{ name: 'TanStack Query', render: <ReactQueryDevtoolsPanel /> }]} />;
 }
