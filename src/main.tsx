15acf8606eb9d68353408328fd3eaf0d881dd33a
Shaun Ziman <devshaun1@gmail.com>
2026-06-26 23:09:10 +0200

    Add @vercel/analytics for enhanced analytics tracking in the application

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx';
 import AuthProvider from './features/auth/AuthProvider.tsx';
 import QueryProvider from './providers/QueryProvider.tsx';
 import DevtoolProvider from './providers/DevtoolProvider.tsx';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
       <AuthProvider>
         <App />
         <DevtoolProvider />
+        <Analytics />
       </AuthProvider>
     </QueryProvider>
   </StrictMode>
